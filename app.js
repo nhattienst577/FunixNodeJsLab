5fbaeeb3d46a495cbd2e0af06c55df1b0e6c3aef
Nhat Tien <nhattienst577@gmail.com>
2022-04-02 15:11:22 +0700

    lab1.4 chuyen huong req

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+//1.4 chuyển hướng request, kết nối req và res
 const http = require("http");
 
 //req : request, res: response
 //tim kiem ham nay va thuc hien moi yeu cau den
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
