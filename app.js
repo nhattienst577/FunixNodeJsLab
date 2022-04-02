430dbb509497fb753ac270cef623abbbd1b19a78
Nhat Tien <nhattienst577@gmail.com>
2022-04-02 22:56:24 +0700

    Lab1.5 request part 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //1.4 chuyển hướng request, kết nối req và res
 const http = require("http");
+const fs = require("fs");
 
 //req : request, res: response
 //tim kiem ham nay va thuc hien moi yeu cau den
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -15,6 +17,12 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
