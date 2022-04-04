7309ac2b01d1ce50eef6e902b246387b086352d7
Nhat Tien <nhattienst577@gmail.com>
2022-04-04 15:19:27 +0700

    Lab2.4

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const http = require("http");
-
 const express = require("express");
-
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
-  next(); //cho phep req duoc den middleware tiep theo
+app.use("/", (req, res, next) => {
+  console.log("This always run");
+  next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
+  res.send("<h1>The Add Product Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In the middleware");
   res.send("<h1>Hello NodeJS</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
