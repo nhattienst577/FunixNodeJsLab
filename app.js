2d60e9fb7583663ba3accad3fed345c651524ef7
Nhat Tien <nhattienst577@gmail.com>
2022-04-04 16:01:43 +0700

    Lab2.5 phan tich cu phap req gui den

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always run");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>The Add Product Page</h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><Button type="submit">Add Product</Button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the middleware");
   res.send("<h1>Hello NodeJS</h1>");
 });
 
