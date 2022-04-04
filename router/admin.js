9ad3a69f548d71261ad06433af4461c891f17516
Nhat Tien <nhattienst577@gmail.com>
2022-04-04 21:49:30 +0700

    update Lab2.9 Phân luồng đường dẫn

--- router/admin.js
+++ router/admin.js
@@ -5,11 +5,11 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><Button type="submit">Add Product</Button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><Button type="submit">Add Product</Button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
