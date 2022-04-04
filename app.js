76a986d1e782ab634db2aab0efd9c730b26d7fda
Nhat Tien <nhattienst577@gmail.com>
2022-04-04 22:21:28 +0700

    Update Lab2.12 404.html

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser"); // duoc sd de xu ly from nhap vao
 
@@ -15,7 +17,7 @@ app.use("/admin", adminRouters);
 app.use(shopRouters);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
