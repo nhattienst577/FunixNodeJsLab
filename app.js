5779ac542fe8f94bc7dd8649ab5deead95384dc2
Nhat Tien <nhattienst577@gmail.com>
2022-04-04 21:10:15 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser"); // duoc sd de xu ly from nhap vao
 
 const app = express();
-
+//là một đối tượng body chứa dữ liệu mà đã được parsed sẽ được đưa vào request (có thể hiểu là req.body).
+//Dữ liệu đó là một cặp key-value, trong đó value có thể là array hoặc string nếu extended: false và các loại còn lại nếu extended: true.
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
