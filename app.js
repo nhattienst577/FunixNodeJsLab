3329eaf00c56ccd743ab9183169d12891a449445
Nhat Tien <nhattienst577@gmail.com>
2022-04-05 21:39:17 +0700

    Lab3.2 pug

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const path = require("path");
-
 const express = require("express");
 const bodyParser = require("body-parser"); // duoc sd de xu ly from nhap vao
-
 const app = express();
-
 const adminData = require("./router/admin");
 const shopRouters = require("./router/shop");
+
+//template engine
+app.set("view engine", "pug");
+app.set("views", "views");
 
 //là một đối tượng body chứa dữ liệu mà đã được parsed sẽ được đưa vào request (có thể hiểu là req.body).
 //Dữ liệu đó là một cặp key-value, trong đó value có thể là array hoặc string nếu extended: false và các loại còn lại nếu extended: true.
