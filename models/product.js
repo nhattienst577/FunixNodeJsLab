eeb7bae714542dcf37954f15021d33727e544274
Nhat Tien <nhattienst577@gmail.com>
2022-04-21 21:18:33 +0700

    Lab4.9 Tải dữ liệu chi tiết sản phẩm

--- models/product.js
+++ models/product.js
@@ -38,4 +38,12 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
+  //chi tai 1 san pham duy nhat
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
+  }
 };
