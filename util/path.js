2f852baf8b4eea109591afceb1e74d7c63a15288
Nhat Tien <nhattienst577@gmail.com>
2022-04-21 20:21:29 +0700

    Lab4.7 Thêm ID sản phẩm vào đường dẫn

--- util/path.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-//dirname trả về tên thư mục của 1 đường dẫn
-//mainModule đề cập đến module chính đã khởi động ứng dụng ở đây là app.js
-
-module.exports = path.dirname(process.mainModule.filename);
