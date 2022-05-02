cd764658a166b5aafd187b309943e0f71d7fb3f9
Nhat Tien <nhattienst577@gmail.com>
2022-05-02 19:43:05 +0700

    Lab5.8 Kết nối với Cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-//connection pool - vung ket noi
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "wwwwwgunner", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "wwwwgunner",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
