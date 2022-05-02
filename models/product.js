f372fc1cf007c5f59778bdf9eebd71cb17b22970
Nhat Tien <nhattienst577@gmail.com>
2022-05-02 19:58:25 +0700

    Lab5.9 Định nghĩa một model

--- models/product.js
+++ models/product.js
@@ -1,33 +1,28 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
 
-const Cart = require("./cart");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+//xac dinh 1 model moi bang cach goi define
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
-
-  //thao tác xóa
-  static deleteById(id) {}
-
-  //thao tác tìm nạp
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  //tìm theo id
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+module.exports = Product;
