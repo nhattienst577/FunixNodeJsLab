6ebf82652b79eba04218bc816c953a16fb5aa3ef
Nhat Tien <nhattienst577@gmail.com>
2022-05-02 20:16:16 +0700

    Lab5.10 Đồng bộ hóa các định nghĩa JS với Cơ sở dữ liệu

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser"); // duoc sd de xu ly from nhap vao
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 const adminRoutes = require("./routes/admin");
@@ -23,5 +23,16 @@ app.use("/admin", adminRoutes);
 app.use(shopRouters);
 
 app.use(errorController.get404);
+
+//sync quan sát được toàn bộ model đã được xác định, nó đồng bộ hóa các model tới csdl bằng cách tạo các bảng thích hợp
+sequelize
+  .sync()
+  .then((result) => {
+    //console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node-complete", "root", "wwwwwgunner", {
+const sequelize = new Sequelize("node-complete", "root", "wwwwgunner", {
   dialect: "mysql",
   host: "localhost",
 });
