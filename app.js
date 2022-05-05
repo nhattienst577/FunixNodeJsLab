430d9bf7f9826c648f67f4d2a7d7cddf1832c003
Nhat Tien <nhattienst577@gmail.com>
2022-05-05 20:38:20 +0700

    Lab6.1 Cài đặt trình điều khiển MongoDB

--- app.js
+++ app.js
@@ -2,21 +2,15 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser"); // duoc sd de xu ly from nhap vao
 
-//routes
-const adminRoutes = require("./routes/admin");
-const shopRouters = require("./routes/shop");
+// routes
+// const adminRoutes = require("./routes/admin");
+// const shopRouters = require("./routes/shop");
 
-//controllers
-const errorController = require("./controllers/error");
+// controllers
+// const errorController = require("./controllers/error");
 
-//database
-const sequelize = require("./util/database");
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const Order = require("./models/order");
-const OrderItem = require("./models/order-item");
+// database
+const mongoConnect = require("./util/database");
 
 //create app
 const app = express();
@@ -33,59 +27,22 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Store user in request
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRouters);
+// app.use("/admin", adminRoutes);
+// app.use(shopRouters);
 
-app.use(errorController.get404);
+// app.use(errorController.get404);
 
-// Create relationship for models
-// user & product --- one to many
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-// user & cart --- one to one
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// cart & product --- many to many
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-// order & user --- one to many
-Order.belongsTo(User);
-User.hasMany(Order);
-//order & product ---many to many
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-//sync quan sát được toàn bộ model đã được xác định, nó đồng bộ hóa các model tới csdl bằng cách tạo các bảng thích hợp
-sequelize
-  //.sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-    //console.log(result);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Max", email: "test@test.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    //console.log(user);
-    return user.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoConnect((client) => {
+  console.log(client);
+  app.listen(3000);
+});
