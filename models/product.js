8257426996a333d7c63ef9ffb59ffbe811d38930
Nhat Tien <nhattienst577@gmail.com>
2022-05-05 21:50:18 +0700

    Lab6.2 Tạo kết nối cơ sở dữ liệu

--- models/product.js
+++ models/product.js
@@ -1,6 +1,15 @@
-const Sequelize = require("sequelize");
+const mongoConnect = require("../util/database");
 
-const sequelize = require("../util/database");
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+  save() {}
+}
 
 //xac dinh 1 model moi bang cach goi define
 const Product = sequelize.define("product", {
