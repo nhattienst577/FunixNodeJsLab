acbb367ee59064c508108ecc0466a976fec12e67
Nhat Tien <nhattienst577@gmail.com>
2022-05-15 20:34:39 +0700

    Lab6.1 Cài đặt trình điều khiển MongoDB

--- util/database.js
+++ util/database.js
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://admin:wwwwgunner@cluster0.83dqc.mongodb.net/nodejs_funix_course?retryWrites=true&w=majority"
+    "mongodb+srv://admin:nhattien123@cluster0.ywhvs.mongodb.net/NodeJs-QLBH?retryWrites=true&w=majority"
   )
     .then((client) => {
       console.log("Connected");
