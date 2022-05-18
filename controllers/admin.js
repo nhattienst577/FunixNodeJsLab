aff483d0113a3bf18d8553a7dca6ab0c6a4a0644
Nhat Tien <nhattienst577@gmail.com>
2022-05-19 00:15:09 +0700

    Lab7.6 Cập nhật sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -63,15 +63,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
-  const product = new Product(
-    updatedTitle,
-    updatedImageUrl,
-    updatedPrice,
-    updatedDesc,
-    prodId
-  );
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.imageUrl = updatedImageUrl;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      return product.save();
+    })
     .then((result) => {
       console.log("UPDATED PRODUCT!!!");
       res.redirect("/admin/products");
@@ -81,7 +80,7 @@ exports.postEditProduct = (req, res, next) => {
 
 // // get all products ==> /admin/products
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
